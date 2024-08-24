43157eb1e33271674efa8cc880e95c8df100e334
marvin <marvin.kafka@gmx.de>
2024-08-24 22:07:11 +0200

    new file

---
+++ order/index.php
@@ -0,0 +1,31 @@
+<?php
+header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+
+require_once("../connection.php");
+
+$requestMethod = $_SERVER['REQUEST_METHOD'];
+
+switch ($requestMethod) {
+    case 'GET':
+        require __DIR__ . '/get.php';
+        break;
+
+    case 'POST':
+        require __DIR__ . '/post.php';
+        break;
+
+    case 'PATCH':
+        require __DIR__ . '/patch.php';
+        break;
+
+    case 'DELETE':
+        require __DIR__ . '/delete.php';
+        break;
+
+    default:
+        http_response_code(405);
+        echo json_encode(["message" => "Method Not Allowed"]);
+        break;
+}
+?>
