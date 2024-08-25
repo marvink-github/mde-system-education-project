f4e03d624e9ebe8bc4088d084c1aae7f1400b396
marvin <marvin.kafka@gmx.de>
2024-08-25 03:25:30 +0200

    +

--- machine/patch.php
+++ machine/patch.php
@@ -1,6 +1,6 @@
 <?php
 header("Content-Type: application/json");
-require_once __DIR__ . '/../connection.php'; // Verbindung zur Datenbank
+require_once __DIR__ . '/../connection.php'; 
 
 $input = json_decode(file_get_contents('php://input'), true);
 $machineId = $input['id'] ?? null;
