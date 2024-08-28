17517d1521510b69a833c5e3f25e667779cca18b
marvink-github <marvin.kafka@gmx.de>
2024-08-28 17:48:36 +0200

    logDB für alle GET auskommentiert

--- getdata.php
+++ getdata.php
@@ -10,7 +10,7 @@ if (!isset($_GET['df_api']) || $_GET['df_api'] != 1) {
     logDB($machineconn, 'ERROR', 'df_api ist ungleich 1');
 };
 
-logDB($machineconn, 'GET', $_GET); 
+// logDB($machineconn, 'GET', $_GET); 
 
 $table = $_GET['df_table'] ?? null;
 
