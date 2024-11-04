b007bd419a064785dd1bfac80b10af7d15545344
marvink-github <marvin.kafka@gmx.de>
2024-11-04 20:31:32 +0100

    +

--- getdata.php
+++ getdata.php
@@ -31,7 +31,7 @@ if ($type === 'kvp') {
             $result = mysqli_query($machineconn, $query);
 
             if (!$result) {
-                logDB($machineconn, 'error', 'Failed to update firmware in database.'); //aa
+                logDB($machineconn, 'error', 'Failed to update firmware in database.'); 
             }
         }
     }
